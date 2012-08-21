893be2254155b3d15f129d946a871dde6821d0fb
William Stevenson <yhvh2000@gmail.com>
2012-08-21 23:41:14 +0100

    (go-stones (go-stone-alist)) outputs a circle S-exp but no conversion of letters to numbers yet. Will probably change this alist about

--- go.el
+++ go.el
@@ -17,7 +17,6 @@
 (defvar go-img-size 500)
 (defvar go-process-buffer "*gnugo*" ) ;; make local?
 
-
 (defun go-process ()
   "Starts the go gtp process"
   (start-process "gnugo" "*gnugo*" "gnugo" "--mode" "gtp"))
@@ -41,66 +40,62 @@
 (defun go-stone-alist ()
   "Returns a list of all stones on board in the form
 '((black ((D . 4) (E . 5))) (white ((G . 3) (H . 4 ))))"
-  
-)
+  '((black (D 4) (E 5)) (white (G 3) (H 4 )))
+  )
 
 (defun go-stones (stones-alist)
-  "Returns a list of circle S-expressions for splicing into svg.  
-Random atm, should read in board state"
-  (let 
+  "Returns a list of circle S-expressions for splicing into svg."
+  (let
       (value)
-    (dolist (i black)
-      (cons 
-       `(circle :cx ,(number-to-string (cons i))
-		:cy ,(number-to-string (cdr i))
-		:r "2.4" 
-		:fill "url(#rg)")
-       value))
-    (dolist (i white)
-      (cons 
-       (circle :cx ,(number-to-string (cons i))
-		:cy ,(number-to-string (cdr i))
-		:r "2.4" 
-		:fill "url(#rg)")
-       value))))
+      (dolist (i (cdr (assoc 'black stones-alist)) value)
+	(setq value (cons
+		     `(circle :cx ,(car i)
+			      :cy ,(cdr i)
+			      :r "2.4"
+			      :fill "url(#rg)")
+		     value)))
+	(dolist (i (cdr (assoc 'white stones-alist)) value)
+	  (setq value (cons
+		       `(circle :cx ,(car i)
+				:cy ,(cdr i)
+				:r "2.4"
+				:fill "url(#rg)")
+		       value)))))
 
-(defun go-img-string ()
-  "Returns a svg string for game image
+    (defun go-img-string ()
+      "Returns a svg string for game image
 Green?
 ...Super Green."
-  (xmlgen 
-   `(svg :xmlns "http://www.w3.org/2000/svg" 
-	 :width ,(number-to-string go-img-size)
-	 :height ,(number-to-string go-img-size)
-	 :encoding "UTF-8" :viewBox "0 0 96 96"
-	 (rect :width "114" :height "114" :fill "#DCB35C")
-	 (path :stroke "#000" :stroke-width ".2" :fill "none" 
-	       :d "M2.9,93h90.2m-.2-5H3m0-5h90m0-5H3m0-5h90m0
+      (xmlgen
+       `(svg :xmlns "http://www.w3.org/2000/svg"
+	     :width ,(number-to-string go-img-size)
+	     :height ,(number-to-string go-img-size)
+	     :encoding "UTF-8" :viewBox "0 0 96 96"
+	     (rect :width "114" :height "114" :fill "#DCB35C")
+	     (path :stroke "#000" :stroke-width ".2" :fill "none"
+		   :d "M2.9,93h90.2m-.2-5H3m0-5h90m0-5H3m0-5h90m0
 -5H3m0-5h90m0-5H3m0-5h90m0-5H3m0-5h90m0-5H3m0-5h90m0-5H3m0-5h90m0
 -5H3m0-5h90m0-5H3m-.1-5h90.2M3,3V93m5,0V3m5,0V93m5,0V3m5,0V93m5,
 0V3m5,0V93m5,0V3m5,0V93m5,0V3m5,0V93m5,0V3m5,0V93m5,0V3m5,0V93m5,
 0V3m5,0V93m5,0V3m5,0V93m5,0V3")
-	 (path :stroke "#000" :stroke-width "2" :stroke-linecap "round" 
-	       :d "M18,78l0,0m30,0l0,0m30,0l0,0m0-30l0,0m-30,0l0,
+	     (path :stroke "#000" :stroke-width "2" :stroke-linecap "round"
+		   :d "M18,78l0,0m30,0l0,0m30,0l0,0m0-30l0,0m-30,0l0,
 0m-30,0l0,0m0-30l0,0m30,0l0,0m30,0l0,0")
-	 (defs
-	   (radialGradient :id "rg" :cx ".3" :cy ".3" :r ".8"
-			   (stop :offset "0" :stop-color "#777")
-			   (stop :offset ".3" :stop-color "#222")
-			   (stop :offset "1" :stop-color "#000"))
-	   (radialGradient :id "wh" :cx ".3" :cy ".3" :r ".8"
-			   (stop :offset "0" :stop-color "#FEE")
-			   (stop :offset ".3" :stop-color "#DDD")
-			   (stop :offset "1" :stop-color "#FFF")))
-	 ,@(go-stones))))
+	     (defs
+	       (radialGradient :id "rg" :cx ".3" :cy ".3" :r ".8"
+			       (stop :offset "0" :stop-color "#777")
+			       (stop :offset ".3" :stop-color "#222")
+			       (stop :offset "1" :stop-color "#000"))
+	       (radialGradient :id "wh" :cx ".3" :cy ".3" :r ".8"
+			       (stop :offset "0" :stop-color "#FEE")
+			       (stop :offset ".3" :stop-color "#DDD")
+			       (stop :offset "1" :stop-color "#FFF")))
+	     ,@(go-stones))))
 
-(defun go-board-insert ()
-  "Insert go board svg image at cursor pos"
-  (insert-image
-   (create-image (go-img-string) 'svg t
-		 :map '(((circle . ((100 . 100) . 20)) 
-			 area1 
-			 (pointer hand))))))
-
-
- 
+    (defun go-board-insert ()
+      "Insert go board svg image at cursor pos"
+      (insert-image
+       (create-image (go-img-string) 'svg t
+		     :map '(((circle . ((100 . 100) . 20))
+			     area1
+			     (pointer hand))))))
