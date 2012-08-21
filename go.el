52ddf66a34ccb3750fd5fe161c9fe733e07b8735
William Stevenson <yhvh2000@gmail.com>
2012-08-21 19:05:51 +0100

    Cleanup and added defcustoms, defgroup and started api defun's

--- go.el
+++ go.el
@@ -1,7 +1,44 @@
-(require 'xmlgen) ;; https://github.com/philjackson/xmlgen
+;; depends xmlgen https://github.com/philjackson/xmlgen
+
+(defgroup go-svg nil
+  "Top level for go-svg customization.")
+
+(defcustom go-program "/usr/bin/gnugo"
+  "Program to "
+  :type '(string)
+  :group 'go-svg)
+
+(defcustom go-program-args "--mode gtp"
+  "Program to "
+  :type '(string)
+  :group 'go-svg)
 
 (defvar boardsize 19)
 (defvar go-img-size 500)
+(defvar go-process-buffer "*gnugo*" ) ;; make local?
+
+
+(defun go-process ()
+  "Starts the go gtp process"
+  (start-process "gnugo" "*gnugo*" "gnugo" "--mode" "gtp"))
+
+(defun go-boardsize (size)
+  "Set boardsize to SIZE and clear the board"
+  (process-send-string go-process-buffer "boardsize 19\n"))
+
+(defun go-play-stone (color pos)
+  "Plays a stone of COLOR at position POS"
+  (process-send-string go-process-buffer (concat "play " color " " pos "\n")))
+
+
+(defun go-list-stones (color)
+  "Returns a list of positions for COLOR"
+  (process-send-string go-process-buffer "list_stones " color " \n"))
+
+(defun go-genmove (color)
+  "Generate and play the supposedly best move for COLOR."
+  (process-send-string go-process-buffer (concat "genmove " color "\n")))
+
 
 (defun go-stones ()
   "Returns a list of circle S-expressions for splicing into svg
@@ -11,11 +48,16 @@ Random atm, should read in board state"
     (dotimes (i 19 value)
       (dotimes (j 19 value)
 	(if (> (random 2) 0)
-	    (setq value (cons `(circle :cx ,(number-to-string (+ 2.9 (* i 5)))
-				       :cy ,(number-to-string (+ 2.9 (* j 5)))
-				       :r "2.4" 
-				       :fill ,(concat "url(#" (if (> (random 2) 0) "rg" "wh") ")")) value)))))))
-  
+	    (setq value 
+		  (cons 
+		   `(circle :cx ,(number-to-string (+ 2.9 (* i 5)))
+			    :cy ,(number-to-string (+ 2.9 (* j 5)))
+			    :r "2.4" 
+			    :fill ,(concat "url(#" 
+					   (if (> (random 2) 0) 
+					       "rg" "wh") ")"))
+		   value)))))))
+
 (defun go-img-string ()
   "Returns a svg string for game image
 Green?
@@ -27,9 +69,14 @@ Green?
 	 :encoding "UTF-8" :viewBox "0 0 96 96"
 	 (rect :width "114" :height "114" :fill "#DCB35C")
 	 (path :stroke "#000" :stroke-width ".2" :fill "none" 
-	       :d "M2.9,93h90.2m-.2-5H3m0-5h90m0-5H3m0-5h90m0-5H3m0-5h90m0-5H3m0-5h90m0-5H3m0-5h90m0-5H3m0-5h90m0-5H3m0-5h90m0-5H3m0-5h90m0-5H3m-.1-5h90.2M3,3V93m5,0V3m5,0V93m5,0V3m5,0V93m5,0V3m5,0V93m5,0V3m5,0V93m5,0V3m5,0V93m5,0V3m5,0V93m5,0V3m5,0V93m5,0V3m5,0V93m5,0V3m5,0V93m5,0V3")
+	       :d "M2.9,93h90.2m-.2-5H3m0-5h90m0-5H3m0-5h90m0
+-5H3m0-5h90m0-5H3m0-5h90m0-5H3m0-5h90m0-5H3m0-5h90m0-5H3m0-5h90m0
+-5H3m0-5h90m0-5H3m-.1-5h90.2M3,3V93m5,0V3m5,0V93m5,0V3m5,0V93m5,
+0V3m5,0V93m5,0V3m5,0V93m5,0V3m5,0V93m5,0V3m5,0V93m5,0V3m5,0V93m5,
+0V3m5,0V93m5,0V3m5,0V93m5,0V3")
 	 (path :stroke "#000" :stroke-width "2" :stroke-linecap "round" 
-	       :d "M18,78l0,0m30,0l0,0m30,0l0,0m0-30l0,0m-30,0l0,0m-30,0l0,0m0-30l0,0m30,0l0,0m30,0l0,0")
+	       :d "M18,78l0,0m30,0l0,0m30,0l0,0m0-30l0,0m-30,0l0,
+0m-30,0l0,0m0-30l0,0m30,0l0,0m30,0l0,0")
 	 (defs
 	   (radialGradient :id "rg" :cx ".3" :cy ".3" :r ".8"
 			   (stop :offset "0" :stop-color "#777")
@@ -42,9 +89,10 @@ Green?
 	 ,@(go-stones))))
 
 
-(insert-image
- (create-image (go-img-string) 'svg t
-	       :map '(((circle . ((100 . 100) . 20)) 
-		       area1 
-		       (pointer hand)))))
- 
+(defun go-board-insert ()
+  ""
+  (insert-image
+   (create-image (go-img-string) 'svg t
+		 :map '(((circle . ((100 . 100) . 20)) 
+			 area1 
+			 (pointer hand))))))
